609bd4a81f6cbf1c4077568922c04c1ed572142a
Joseff <joseffnic@gmail.com>
2025-10-11 17:52:06 +0200

    Initial commit — fresh history

---
+++ _next/static/chunks/app/other/page-49faa0236aac7ca5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[883],{9159:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.bind(n,2354)),Promise.resolve().then(n.bind(n,1427))}},e=>{e.O(0,[883],()=>{[33,323,315,991].map(e.E)},5),e.O(0,[831,248,354,441,964,358],()=>e(e.s=9159)),_N_E=e.O()}]);
