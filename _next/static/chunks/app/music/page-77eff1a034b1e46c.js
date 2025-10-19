c1529be2aba5985246b3315d315e7ce4ff7ed083
Joseff <joseffnic@gmail.com>
2025-10-19 21:32:05 +0200

    Deploy

---
+++ _next/static/chunks/app/music/page-77eff1a034b1e46c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[466],{5669:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.bind(n,1039)),Promise.resolve().then(n.bind(n,2617)),Promise.resolve().then(n.bind(n,1868))}},e=>{e.O(0,[466],()=>{[33,323,315,270].map(e.E)},5),e.O(0,[868,356,842,39,531,441,255,358],()=>e(e.s=5669)),_N_E=e.O()}]);
